0ef92ef62e668716d8a9d17fa545607695bc4563
alexjdanoff <alex75126@gmail.com>
2022-02-06 01:30:33 +0200

    Создал 4 кастомных ячеек вместо 1

---
+++ VKClient_АлександрЖданов/Views/NewsTextTableViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  NewsTextTableViewCell.swift
+//  VKClient_АлександрЖданов
+//
+//  Created by Alexandru Jdanov on 03.02.2022.
+//
+
+import UIKit
+
+class NewsTextTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var textView: UITextView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+    
+}
