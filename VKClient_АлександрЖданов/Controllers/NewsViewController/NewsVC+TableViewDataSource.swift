3082e1e4bbce9a5e7c1c78ec4458ef5270d1855b
alexjdanoff <alex75126@gmail.com>
2022-02-08 01:24:47 +0200

    Правки по прошлому ДЗ

--- VKClient_АлександрЖданов/Controllers/NewsViewController/NewsVC+TableViewDataSource.swift
+++ VKClient_АлександрЖданов/Controllers/NewsViewController/NewsVC+TableViewDataSource.swift
@@ -12,20 +12,48 @@ extension NewsVC: UITableViewDataSource {
     func configureTableView() {
         tableView.dataSource = self
         tableView.delegate = self
-        tableView.register(UINib(nibName: "CustomTableViewCellNews", bundle: nil), forCellReuseIdentifier: customCellNewsReuseIdentifier)
+        tableView.register(UINib(nibName: "NewsTitleTableViewCell", bundle: nil), forCellReuseIdentifier: "NewsTitleTableViewCell")
+        tableView.register(UINib(nibName: "NewsTextTableViewCell", bundle: nil), forCellReuseIdentifier: "NewsTextTableViewCell")
+        tableView.register(UINib(nibName: "NewsImageTableViewCell", bundle: nil), forCellReuseIdentifier: "NewsImageTableViewCell")
+        tableView.register(UINib(nibName: "NewsLikeTableViewCell", bundle: nil), forCellReuseIdentifier: "NewsLikeTableViewCell")
     }
     
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        
+    func numberOfSections(in tableView: UITableView) -> Int {
         return newsArray.count
     }
     
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 4
+    }
+    
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cellsIndex = indexPath.row
         
-        let cell = tableView.dequeueReusableCell(withIdentifier: customCellNewsReuseIdentifier, for: indexPath) as! CustomTableViewCellNews
-        
-        cell.configure(news: newsArray[indexPath.row])
-
-        return cell
+        switch cellsIndex {
+        case 0:
+            let titleCell: NewsTitleTableViewCell = tableView.dequeueReusableCell(forIndexPath: indexPath)
+            
+            titleCell.configure(image: newsArray[indexPath.section].avatar, title: newsArray[indexPath.section].name, date: newsArray[indexPath.section].date)
+            
+            return titleCell
+        case 1:
+            let textCell: NewsTextTableViewCell = tableView.dequeueReusableCell(forIndexPath: indexPath)
+            
+            textCell.configure(text: newsArray[indexPath.section].text)
+            
+            return textCell
+        case 2:
+            let imageCell: NewsImageTableViewCell = tableView.dequeueReusableCell(forIndexPath: indexPath)
+            
+            imageCell.configure(image: newsArray[indexPath.section].image)
+            
+            return imageCell
+        default:
+            let likeCell: NewsLikeTableViewCell = tableView.dequeueReusableCell(forIndexPath: indexPath)
+            
+            likeCell.configure(likeCount: newsArray[indexPath.section].likeCount, commentCount: newsArray[indexPath.section].commentCount)
+            
+            return likeCell
+        }
     }
 }
---
+++ VKClient_АлександрЖданов/Extensions/ReusableIdentifier.swift
@@ -0,0 +1,38 @@
+//
+//  ReusableIdentifier.swift
+//  VKClient_АлександрЖданов
+//
+//  Created by Alexandru Jdanov on 07.02.2022.
+//
+
+import UIKit
+
+protocol ReusableView: AnyObject {}
+
+extension ReusableView where Self: UIView {
+    static var reuseIdentifier: String {
+        return String(describing: self)
+    }
+}
+
+extension UITableViewCell: ReusableView { }
+
+extension UITableView {
+    func dequeueReusableCell<T: UITableViewCell>(forIndexPath indexPath: IndexPath) -> T {
+        guard let cell = dequeueReusableCell(withIdentifier: T.reuseIdentifier, for: indexPath) as? T else {
+            fatalError("Could not dequeue cell with identifier: \(T.reuseIdentifier)")
+        }
+        
+        return cell
+    }
+}
+
+
+
+
+
+
+
+
+
+
